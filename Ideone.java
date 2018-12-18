72521dcad9262a59e784523c19afdbdcbe5066e3
mahisha1997 <42768422+mahisha1997@users.noreply.github.com>
2018-12-18 11:40:02 +0530

    Create Ideone.java

---
+++ Ideone.java
@@ -0,0 +1,18 @@
+
+import java.io.*;
+class Ideone
+{
+public static void main(String[]args)
+throws java.lang.Exception
+{
+int num;
+Scanner scan =new Scanner(System.in);
+num = scan.nextInt();
+{
+if(num % 2==0)
+System.out.println("even");
+else
+System.out.println("odd");
+}
+}
+}
