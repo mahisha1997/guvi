25e475b2ae6823ac3013a7c6166b5f214e01b4d0
mahisha1997 <42768422+mahisha1997@users.noreply.github.com>
2018-12-18 12:24:10 +0530

    Create vow.java

---
+++ vow.java
@@ -0,0 +1,19 @@
+import java.io.;
+import java.util.;
+class vowels
+{
+public static void main(string[]args)
+{
+Scanner s=new Scanner(System.in);
+char[] arr=new int('a','e','i','o','u');
+char c=s.next();
+if(c==arr)
+{
+System.out.println("vowels");
+}
+else
+{
+System.out.println("consonants");
+}
+}
+}
